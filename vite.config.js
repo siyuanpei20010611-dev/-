7318c6800a598ee44a5097273faa180db0d5765f
siyuanpei20010611-dev <siyuanpei20010611@gmail.com>
2026-03-02 23:25:21 +0800

    Update Vite config to set base path and format plugins
    Set base path for deployment and formatted plugins array.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  // 这里的 base 必须设置为相对路径 './' 或者你的仓库名 '/siyuanpei20010611-dev/'
+  base: './', 
 })
